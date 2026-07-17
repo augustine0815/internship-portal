9bcbaedbe3ea292bf68e34d647fef5b0dfdb3a50
Augustine Lee Siew Peng <augustine010815@gmail.com>
2026-07-17 16:17:13 +0800

    feat: production deployment complete

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://13.212.52.253:5000/api',
 });
 
 // Automatically attach token to every request
